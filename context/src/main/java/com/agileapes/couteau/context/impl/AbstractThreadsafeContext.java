8565ac37ae6ec2eb799ba1fa169ba93bb3471ae2
mmnaseri <m.m.naseri@gmail.com>
2013-06-27 16:42:48 +0430

    Wrote the context module

---
+++ context/src/main/java/com/agileapes/couteau/context/impl/AbstractThreadsafeContext.java
@@ -0,0 +1,42 @@
+package com.agileapes.couteau.context.impl;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author Mohammad Milad Naseri (m.m.naseri@gmail.com)
+ * @since 1.0 (6/27/13, 4:33 PM)
+ */
+public abstract class AbstractThreadSafeContext<E> extends AbstractContext<E> {
+
+    private final Map<String, E> map = new ConcurrentHashMap<String, E>();
+
+    @Override
+    protected boolean write(String name, E item) {
+        map.put(name, item);
+        return contains(name);
+    }
+
+    @Override
+    protected E read(String name) {
+        return map.get(name);
+    }
+
+    @Override
+    public boolean contains(String name) {
+        return map.containsKey(name);
+    }
+
+    @Override
+    public Collection<E> getBeans() {
+        return Collections.unmodifiableCollection(map.values());
+    }
+
+    @Override
+    public Collection<String> getBeanNames() {
+        return Collections.unmodifiableCollection(map.keySet());
+    }
+
+}
