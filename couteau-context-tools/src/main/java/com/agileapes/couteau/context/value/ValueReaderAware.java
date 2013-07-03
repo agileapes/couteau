26f20a3c2464276bbc937ccdf940a018c07c5d8f
mmnaseri <m.m.naseri@gmail.com>
2013-07-03 11:51:26 +0430

    Adapted DI interface for ValueReader

---
+++ couteau-context-tools/src/main/java/com/agileapes/couteau/context/value/ValueReaderAware.java
@@ -0,0 +1,11 @@
+package com.agileapes.couteau.context.value;
+
+/**
+ * @author Mohammad Milad Naseri (m.m.naseri@gmail.com)
+ * @since 1.0 (7/3/13, 11:43 AM)
+ */
+public interface ValueReaderAware {
+
+    void setValueReader(ValueReader valueReader);
+
+}
